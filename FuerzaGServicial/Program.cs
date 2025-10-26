dfe5afeb21fdd35535b464b14955670c9c9bcc96
gabriel <patosquare418@gmail.com>
2025-10-25 23:09:48 -0400

    feat: OwnerService

--- FuerzaGServicial/Program.cs
+++ FuerzaGServicial/Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+//Authentication management
+builder.Services
+    .AddAuthentication("GForceAuth")
+    .AddCookie("GForceAuth", options =>
+    {
+        options.Cookie.Name = "GForceCookie";
+        options.LoginPath = "/Login";
+        options.AccessDeniedPath = "/AccessDenied"; //TODO
+        options.LogoutPath = "/Logout"; //TODO
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    });
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
